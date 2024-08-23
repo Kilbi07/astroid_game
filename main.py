0790c0ad84bbe152aa6cb5b58b65a1f412d38449
Kilbi07 <140620327+Kilbi07@users.noreply.github.com>
2024-08-23 18:17:19 +0200

    Add logic for player model

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
