78b7bc6d11a9afb7f808441d3d5acea5ec8ae219
Kilbi07 <140620327+Kilbi07@users.noreply.github.com>
2024-10-02 18:42:44 +0200

    Add shot timer

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
         self.position = pygame.Vector2(x,y)
         self.radius = PLAYER_RADIUS
         self.rotation = 0
+        self.shot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -25,6 +26,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.shot_timer -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-1*dt)
@@ -35,13 +37,18 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-1*dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+                self.shoot()
+        
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.shot_timer > 0:
+            return
         shot = Shot(self.position.x,self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
+        
 
